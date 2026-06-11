ff58610be63562d1f5ea0a3c1a20bd0f788728eb
Lukaproka1982 <lukajanosevic52@gmail.com>
2026-06-11 14:53:26 +0200

    POPRAVLJENI DEKODER, sad ima dinamicki alociran niz

--- Dekoder_LZW.c
+++ Dekoder_LZW.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#define N 4096
+int N=4096;
 unsigned long r_buffer = 0;
 int r_bit_count = 0;
 void FREE_TABLE(char* table[],int codes);
@@ -24,74 +24,97 @@ int ReadBits(FILE* f,int length) {
      return kod;
 }
 void ADD_CHARS(char* table[]) {
+     if (table==NULL) {
+          exit(-1);
+     }
      for(int i=0;i<256;i++) {
           table[i]=calloc(2,sizeof(char));
           table[i][0]=(char)i;
      }
 }
 void ADD_STRING(char* table[],char str[],int code) {
+     if (table==NULL) {
+          exit(-1);
+     }
      table[code]=calloc(strlen(str)+1,sizeof(char));
      strcpy(table[code],str);
 }
-void LZW_DECODE(char* table[],char filename_read[],char filename_write[]) {
+void LZW_DECODE(char*** table,char filename_read[],char filename_write[]) {
      int code=256;
      int code_length=8,new_code;
-     char* string=NULL,*character=malloc(sizeof(char));
+     if (table==NULL) {
+          exit(-1);
+     }
+     char character[2] = {0};
      FILE *f=fopen(filename_read,"rb");
      FILE *out=fopen(filename_write,"w");
      int old_code=ReadBits(f,code_length);
-     fprintf(out, "%s", table[old_code]);
+     fprintf(out, "%s", (*table)[old_code]);
      code_length++;
      while((new_code = ReadBits(f, code_length)) != -1) {
           char*temp=NULL;
-          if (table[new_code]==NULL) {
-               temp=realloc(string,(strlen(table[old_code])+2)*sizeof(char));
+          if ((*table)[new_code]==NULL) {
+               temp=malloc((strlen((*table)[old_code])+2)*sizeof(char));
                if (temp==NULL) {
                     free(temp);
                     fclose(f);
                     fclose(out);
-                    FREE_TABLE(table,code);
+                    FREE_TABLE(*table,code);
                     exit(-1);
                }
-               strcpy(temp,table[old_code]);
+               strcpy(temp,(*table)[old_code]);
                strcat(temp,character);
           }
           else {
-               temp=realloc(string,(strlen(table[new_code])+1)*sizeof(char));
+               temp=malloc((strlen((*table)[new_code])+1)*sizeof(char));
                if (temp==NULL) {
                     free(temp);
                     fclose(f);
                     fclose(out);
-                    FREE_TABLE(table,code);
+                    FREE_TABLE((*table),code);
                     exit(-1);
                }
-               strcpy(temp,table[new_code]);
+               strcpy(temp,(*table)[new_code]);
                temp[strlen(temp)]='\0';
           }
           fprintf(out, "%s", temp);
-          *character=temp[0];
-          char*novi=malloc((strlen(table[old_code]) + 2) * sizeof(char));
-          strcpy(novi,table[old_code]);
+          character[0]=temp[0];
+          char*novi=malloc((strlen((*table)[old_code]) + 2) * sizeof(char));
+          strcpy(novi,(*table)[old_code]);
           strcat(novi,character);
-          ADD_STRING(table,novi,code++);
+          if (code >= N) {
+               int old_N=N;
+               N=N*2;
+               char** temp_table=realloc(*table, N * sizeof(char*));
+               if (temp_table == NULL) {
+                    exit(-1);
+               }
+               *table=temp_table;
+               for(int i=old_N;i<N;i++) {
+                    (*table)[i]=NULL;
+               }
+          }
+          ADD_STRING(*table,novi,code++);
           free(novi);
           free(temp);
           if ((1<<code_length)==code) code_length++;
           old_code = new_code;
      }
-     free(string);
-     free(character);
      fclose(f);
      fclose(out);
-     FREE_TABLE(table,code);
+     FREE_TABLE(*table,code);
 }
 void FREE_TABLE(char* table[],int codes) {
-     for (int i=0;i<codes;i++) {
+     if (table==NULL) {
+          exit(-1);
+     }
+     for (int i=0;i<N;i++) {
           free(table[i]);
      }
+     free(table);
 }
 int main() {
-     char* decoder[N];
+     char** decoder=calloc(N,sizeof(char*));
      ADD_CHARS(decoder);
-     LZW_DECODE(decoder,"coded.txt","decoded.txt");
+     LZW_DECODE(&decoder,"coded.txt","decoded.txt");
 }
